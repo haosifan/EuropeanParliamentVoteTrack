92eaa12c2cea96718d559d58ab2d30fd3a285873
Stefan Haußner <stefan.haussner@uni-due.de>
2019-06-11 18:05:43 +0200

    tidy up 01_scrape_ep_website.R

--- scripts/01_scrape_ep_website.R
+++ scripts/01_scrape_ep_website.R
@@ -1,22 +1,25 @@
 library(rvest)
 library(purrr)
 
-url <- "https://www.europarl.europa.eu/RegistreWeb/search/simple.htm?codeTypeDocu=PPVD&lg=DE&currentPage=200"
+url <- "https://www.europarl.europa.eu/RegistreWeb/search/simple.htm?codeTypeDocu=PPVD&lg=DE&currentPage=1"
+
+temp_url <- url
+history <- NULL
+
+for(i in 1:300) {
+  history[i] <- temp_url
+  try(
+    temp_url <- html_session(temp_url) %>% 
+      follow_link("Next") %>% .$url
+  )
+}
+
+history <- unique(history)
+
+
 
 links <- read_html(url) %>% 
   html_nodes("[class=\"results\"]") %>% 
   html_nodes("a") %>%
   html_attr("href")
 
-temp_url <- url
-history <- NULL
-
-for(i in 1:6) {
-  history[i] <- temp_url
-  try(
-    temp_url <- html_session(temp_url) %>% 
-      follow_link("Next") %>% .$url
-    )
-}
-
-history <- unique(history)
