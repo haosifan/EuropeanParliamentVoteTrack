965053c7926025226a60ade5d60105445e5be344
Stefan Haußner <stefan.haussner@uni-due.de>
2019-07-25 09:43:58 +0200

    Workshop 25.07.

--- scripts/01_scrape_ep_website.R
+++ scripts/01_scrape_ep_website.R
@@ -1,5 +1,6 @@
 library(rvest)
-library(purrr)
+library(plyr)
+library(tidyverse)
 
 url <- "https://www.europarl.europa.eu/RegistreWeb/search/simple.htm?codeTypeDocu=PPVD&lg=DE&currentPage=1"
 
@@ -14,12 +15,17 @@ for(i in 1:300) {
   )
 }
 
-history <- unique(history)
+pages_ep_website <- unique(history)
+saveRDS(pages_ep_website, "temp_files/pages_ep_website.Rdata")
 
+scrape_xml_links <- function(pages) {
+  temp_xml_links<- read_html(pages) %>% 
+    html_nodes("#mainzone a:nth-child(2)") %>% 
+    html_attr("href")
+  return(temp_xml_links)
+}
 
+xml_links <- plyr::ldply(pages_ep_website[1:10], .fun = scrape_xml_links, .progress = "text") %>% 
+  unlist() %>% unname()
 
-links <- read_html(url) %>% 
-  html_nodes("[class=\"results\"]") %>% 
-  html_nodes("a") %>%
-  html_attr("href")
 
